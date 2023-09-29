02c38e05282f95165204fdb0a906fcece41f6870
KyleKeenanGMIT <kylekeenan@live.co.uk>
2023-09-29 10:36:16 +0100

    ex3 done

--- ex3.js
+++ ex3.js
@@ -1,25 +1,36 @@
-let myTasks = [];
+let myTasks = [];//my array
 
-let addTasks=(task) => {
+let addTasks=(task) => {//arrow func
 let length = myTasks.push(task);
 console.log("Item" + task + "has been added to Tasks");
-return length;
+return length;//adding item to tasks var
 }
 
 let listAllTasks = ()=>{
     myTasks.forEach((item)=>{
-console.log(item);
+console.log(item);//lists all tasks i nthe array
     
 });
 }
 
+let deleteTask = (task)=>{//delete task method removing
+   let index = myTasks.indexOf(task);
+   if(index > -1){
+   myTasks.splice(index, 1);
+   console.log("item" + task + "has been removed");
+   }else{
+   console.log("item" + task + "was not found in tasks"); // returns this if index unable to find task.
+   }
+   return myTasks.length;//returns the length of mytasks
+
+}
+
+
 addTasks("Learn JS!");
+addTasks("Learn React!");
 listAllTasks();
+deleteTask("Learn Js!")
 
 
- deleteTask=()=>{
-     let index = myTasks.indexOf(task);
-     myTasks.splice(index,1);
- }
-
+ 
 
