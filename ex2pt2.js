01cda7435f600fb8dc287ae552667f4610bdc627
KyleKeenanGMIT <kylekeenan@live.co.uk>
2023-09-29 10:41:01 +0100

    added folder called lab 1

--- ex2pt2.js
+++
@@ -1,7 +0,0 @@
-
-// user comment
-const Part2 = (myValue) => {
-    return myValue;//arrow function with parameter of my value
-}
-console.log(Part2("Hello")); // myvalue = hello, prints to console.
-
