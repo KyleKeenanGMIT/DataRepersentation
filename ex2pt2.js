ca030f56e373798b123957f521f5bf40c8453e47
KyleKeenanGMIT <kylekeenan@live.co.uk>
2023-09-29 10:03:35 +0100

    ex2pt4 completed

---
+++ ex2pt2.js
@@ -0,0 +1,7 @@
+
+// user comment
+const Part2 = (myValue) => {
+    return myValue;
+}
+console.log(Part2("Hello"));
+
---
+++ ex2pt3.js
@@ -0,0 +1,5 @@
+let Addition = (num1, num2) =>{
+    return num1 + num2;
+}
+
+console.log(Addition(2,3));
