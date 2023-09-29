2eca5f9682dcf03ed34675cdac70d9a81d7434ab
KyleKeenanGMIT <kylekeenan@live.co.uk>
2023-09-29 10:40:29 +0100

    added files to lab 1 folder

---
+++ Lab 1/ex2pt1.js
@@ -0,0 +1,23 @@
+console.log("Hello");
+
+const sayHi = function Hello(){
+    console.log("hi");
+}
+
+let sayBye = ()=>{
+    console.log("bye");
+}
+
+
+sayBye();
+
+let test = (myValue)=>{
+    console.log(myValue);
+}
+
+test("bonjour");
+
+const DataRep=()=> {//data rep arrow function, prints data rep & querying to console.
+    console.log ("Data Repersentation & Querying");
+}
+DataRep();
---
+++ Lab 1/ex2pt2.js
@@ -0,0 +1,7 @@
+
+// user comment
+const Part2 = (myValue) => {
+    return myValue;//arrow function with parameter of my value
+}
+console.log(Part2("Hello")); // myvalue = hello, prints to console.
+
