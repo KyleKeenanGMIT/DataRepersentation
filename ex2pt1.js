ea232cef0d9763454963e2df4f313516d4074b1c
KyleKeenanGMIT <kylekeenan@live.co.uk>
2023-09-29 09:29:13 +0100

    set up through cmd prompt and added basic code

---
+++ ex2pt1.js
@@ -0,0 +1,18 @@
+console.log("Hello");
+
+const sayHi = function Hello(){
+    console.log("hi");
+}
+
+let sayBye = ()=>{
+    console.log("bye");
+}
+
+
+sayBye();
+
+let test = (myValue)=>{
+    console.log(myValue);
+}
+
+test("bonjour");
